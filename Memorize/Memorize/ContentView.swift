9959841f8d64325a78abf6f677b10f1acb9ab466
Nícollas Garibaldi <143560192+nicollasgaribaldi@users.noreply.github.com>
2023-12-26 23:29:14 -0300

    First project commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Nícollas Garibaldi on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    @State var emojiCount = 20
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.blue)
+        }
+        .padding(.horizontal)
+    }
+}
+    
+struct CardView: View {
+        var content: String
+        @State var isFaceUp: Bool = true
+    
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20)
+                if isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.stroke(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else {
+                    shape.fill()
+                }
+            }
+            .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
