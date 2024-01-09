a6a8b8d378ce8f295bff2df89f287f8a37bd8d19
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2024-01-08 21:32:15 -0300

    Lecture 6 4/4

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -37,7 +37,8 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawningConstants.lineWidth)
-                    Circle().padding(5).opacity(0.5)
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+                        .padding(5).opacity(0.5)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
