2d1bc6f85bccb5b1f9f33d3388a21e5378908b0b
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2024-01-14 16:41:30 -0300

    Lecture 10

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -135,11 +135,11 @@ extension CGSize {
     static func -(lhs: Self, rhs: Self) -> CGSize {
         CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
     }
-    static func *(lhs: Self, rhs: Self) -> CGSize {
-        CGSize(width: lhs.width * rhs.width, height: lhs.height * rhs.height)
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
     }
-    static func /(lhs: Self, rhs: Self) -> CGSize {
-        CGSize(width: lhs.width / rhs.width, height: lhs.height / rhs.height)
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width/rhs, height: lhs.height/rhs)
     }
 }
 
