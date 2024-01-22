ea395a78b535f5d4d3d354b9b71a1fbcb2c6f7d5
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2024-01-22 00:34:24 -0300

    Lecture 12 (4/4)

--- EmojiArt/EmojiArt/PaletteManager.swift
+++ EmojiArt/EmojiArt/PaletteManager.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
             NavigationView {
@@ -19,12 +22,35 @@ struct PaletteManager: View {
                             Text(palette.name)
                             Text(palette.emojis)
                         }
+                        .gesture(editMode == .active ? tap : nil)
                     }
+                }
+                .onDelete { indexSet in
+                    store.palettes.remove(atOffsets: indexSet)
+                }
+                .onMove { indexSet, newOffset in
+                    store.palettes.move(fromOffsets: indexSet, toOffset: newOffset)
                 }
             }
             .navigationTitle("Manage Palettes")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem { EditButton() }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented,
+                        UIDevice.current.userInterfaceIdiom != .pad {
+                         Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            }
+            .environment(\.editMode, $editMode)
         }
+    }
+    
+    var tap: some Gesture {
+        TapGesture().onEnded { }
     }
 }
 
@@ -33,5 +59,6 @@ struct PaletteManager_Previews: PreviewProvider {
         PaletteManager()
             .previewDevice("iPhone 8")
             .environmentObject(PaletteStore(named: "Preview"))
+            .preferredColorScheme(.light)
     }
 }
